1b3c2b17c96d422750e3c94cc1d60dadec62a8b5
Abhi834929 <39688478+Abhi834929@users.noreply.github.com>
2018-08-19 20:46:19 +0530

    Add files via upload

---
+++ Java 8 New Features/DefaultMethodEx3.java
@@ -0,0 +1,18 @@
+interface Vehicle{
+	default public void disp(){
+		System.out.println("I am vehicle");
+	}
+}
+
+class Car implements Vehicle{
+	/*public void disp(){
+		System.out.println("I am car");
+	}*/
+}
+
+public class DefaultMethodEx3 {
+	public static void main(String[] args) {
+		Car obj = new Car();
+		obj.disp();
+	}
+}
